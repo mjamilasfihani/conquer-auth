d88dbc290e0a6173bb94b704ee2d84f2a4fa0eed
Mohammad Jamil Asfihani <mohammadjamilasfihani@gmail.com>
2022-10-25 15:38:05 +0700

    Update ConquerHelperTest.php

--- tests/helpers/ConquerHelperTest.php
+++ tests/helpers/ConquerHelperTest.php
@@ -22,10 +22,10 @@ final class ConquerHelperTest extends ConquerAuthTestCase
 
         $this->setPrivateProperty($obj, $property, $value);
 
-        $decryptedStr = 'test';
-        $encryptedStr = 'f00ccb1980b5cea5d1fcd31c47a82686bd3d9b61a8176b9d563dc723cc6ab1d180bc302cc0e11a46a946ed37bdf5362f33e70e94b0fa449b8ffa4614152d81de32fc94e1600bb4918411ec9b7e281e7e4f1909e4';
+        $storedStr = str_decrypt('f00ccb1980b5cea5d1fcd31c47a82686bd3d9b61a8176b9d563dc723cc6ab1d180bc302cc0e11a46a946ed37bdf5362f33e70e94b0fa449b8ffa4614152d81de32fc94e1600bb4918411ec9b7e281e7e4f1909e4');
+        $passedStr = str_decrypt(str_encrypt('test'));
 
-        $this->assertSame(str_decrypt($encryptedStr), $decryptedStr);
-        $this->assertSame(str_decrypt(str_encrypt($decryptedStr)), $decryptedStr);
+        $this->assertSame($storedStr, 'test');
+        $this->assertSame($passedStr, 'test');
     }
 }
