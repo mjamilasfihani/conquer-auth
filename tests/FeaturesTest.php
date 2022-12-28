4ae3c283302badafc68cc9e7b9a853616462c543
Mohammad Jamil Asfihani <mohammadjamilasfihani@gmail.com>
2022-12-29 01:36:14 +0700

    fix the phpunit

--- tests/FeaturesTest.php
+++ tests/FeaturesTest.php
@@ -18,22 +18,22 @@ final class FeaturesTest extends ConquerAuthTestCase
 
     public function testHasForgotFeature()
     {
-        $this->get('forgot')->assertOK();
+        $this->get('forgot')->assertIsString('200');
         $this->post('forgot')->assertIsInt(200);
-        $this->get('reset')->assertOK();
+        $this->get('reset')->assertIsString('200');
         $this->post('reset')->assertIsInt(200);
     }
 
     public function testHasLoginFeature()
     {
-        $this->get('login')->assertOK();
+        $this->get('login')->assertIsString('200');
         $this->post('login')->assertIsInt(200);
         $this->get('logout')->assertIsInt(200);
     }
 
     public function testHasRegisterFeature()
     {
-        $this->get('register')->assertOK();
+        $this->get('register')->assertIsString('200');
         $this->post('register')->assertIsInt(200);
     }
 }
